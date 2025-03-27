3327039bae091b18997690d879ca8c064c814dde
egafadhilla <egafadhilla88@gmail.com>
2025-03-27 15:35:40 +0700

    tambah pycharm project

---
+++ belajarPython/pycharm/main.py
@@ -0,0 +1,15 @@
+print("hello world")
+ukuran = len("aaabbbcccddd")
+panjang = 500
+lebar = 10.56
+kondisi = True
+nama = "bambang prabowo"
+print(ukuran)
+print(type(ukuran))
+print(type(panjang))
+print(type(lebar))
+print(type(kondisi))
+print(type(nama))
+print(88 // 7)
+print(88 / 7)
+print(88 % 7)
